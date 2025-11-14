d713f1bc3394c01af7c9a71da26e71689e002775
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-11-14 14:27:13 -0300

    add keycloak import

--- LedgerFlow.AspireHost/AppHost.cs
+++ LedgerFlow.AspireHost/AppHost.cs
@@ -1,24 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sqlServer = builder.AddSqlServer("sqlserver", port: 1433)
-    .WithLifetime(ContainerLifetime.Persistent)
-    .WithDataVolume();
-
-var database = sqlServer.AddDatabase("LedgerFlow");
+var database = builder.AddSqlServer("sqlserver", port: 1433)
+    //.WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .AddDatabase("LedgerFlow");
 
 var redis = builder.AddRedis("redis");
 
 var keycloak = builder.AddKeycloakContainer("keycloak", port: 2000)
     .WithDataVolume()
-    .WithEnvironment("KEYCLOAK_ADMIN", "admin")
-    .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin");
+    .WithEnvironment("KC_BOOTSTRAP_ADMIN_USERNAME", "admin")
+    .WithEnvironment("KC_BOOTSTRAP_ADMIN_PASSWORD", "admin")
+    .WithImport("import");
 
 var transactionApi = "transactions-api";
 builder.AddProject<Projects.LedgerFlow_Transactions_WebApi>(transactionApi)
     .WithReference(database).WaitFor(database)
     .WithReference(redis)
     .WithReference(keycloak)
-    .WithHttpEndpoint(name: transactionApi, port: 2004);
+    .WithHttpEndpoint(name: transactionApi, port: 2002);
 
 var ledgersummariesApi = "ledgersummaries-api";
 builder.AddProject<Projects.LedgerFlow_LedgerSummaries_WebApi>(ledgersummariesApi)
