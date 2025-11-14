1315f0030f73b51d2bb483451c8488a8284a401b
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-11-14 15:01:43 -0300

    add sql-password

--- LedgerFlow.AspireHost/AppHost.cs
+++ LedgerFlow.AspireHost/AppHost.cs
@@ -2,13 +2,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var database = builder.AddSqlServer("sqlserver", port: 1433)
     //.WithLifetime(ContainerLifetime.Persistent)
-    .WithDataVolume()
+    .WithDataVolume("aspire_sqlserver_data")
     .AddDatabase("LedgerFlow");
 
-var redis = builder.AddRedis("redis");
+var redis = builder.AddRedis("redis")
+    .WithDataVolume("aspire_redis_data");
 
 var keycloak = builder.AddKeycloakContainer("keycloak", port: 2000)
-    .WithDataVolume()
+    .WithDataVolume("aspire_keycloak_data")
     .WithEnvironment("KC_BOOTSTRAP_ADMIN_USERNAME", "admin")
     .WithEnvironment("KC_BOOTSTRAP_ADMIN_PASSWORD", "admin")
     .WithImport("import");
@@ -16,7 +17,6 @@ var keycloak = builder.AddKeycloakContainer("keycloak", port: 2000)
 var transactionApi = "transactions-api";
 builder.AddProject<Projects.LedgerFlow_Transactions_WebApi>(transactionApi)
     .WithReference(database).WaitFor(database)
-    .WithReference(redis)
     .WithReference(keycloak)
     .WithHttpEndpoint(name: transactionApi, port: 2002);
 
